bee6928662b940dc9a690ae93c8ff04f1f02a414
gouf <innocent.zero@gmail.com>
2020-07-31 18:57:07 +0900

    refactor: gon の設定値を Hash に纏める

--- app/controllers/growth_curve_sample_controller.rb
+++ app/controllers/growth_curve_sample_controller.rb
@@ -1,31 +1,29 @@
 class GrowthCurveSampleController < ApplicationController
+  BORDER_COLOR_RED = 'rgba(255, 0, 0, 1)'
+  BORDER_COLOR_BLUE = 'rgba(54, 162, 235, 1)'
+
   def index
-    # 仮で固定値
-    height = [46, 51, 55, 59, 62, 63, 64, 65.5, 66, 66.5, 71, 69.5]
-    # 12.times.map { (30..70).to_a.sample }
-    # gon.height = height
-    gon.height = 12.times.map { (40..70).to_a.sample }
+    gon_set_values = {
+      # 仮で固定値
+      height: [46, 51, 55, 59, 62, 63, 64, 65.5, 66, 66.5, 71, 69.5],
+      weight: [2.26, 3.445, 4.62, 5, 6.05, 6.64, 7.1, 7.4, 7.5, 7.6, 7.8, 7.5],
+      # 描画色の設定
+      borderRed: BORDER_COLOR_RED,
+      borderBlue: BORDER_COLOR_BLUE
+    }
 
-    # NOTE: chart-js で値の数だけ色指定が必要
-    border_red = 'rgba(255, 0, 0, 1)'
-    gon.borderRed = Array.new(height.size) { border_red }
-
-    # -----------------------------------------
-
-    # 仮で固定値
-    weight = [2.26, 3.445, 4.62, 5, 6.05, 6.64, 7.1, 7.4, 7.5, 7.6, 7.8, 7.5]
-    gon.weight = weight
-
-    # NOTE: chart-js で値の数だけ色指定が必要
-    border_blue = 'rgba(54, 162, 235, 1)'
-    # gon.borderBlue = Array.new(weight.size) { border_blue }
-    gon.borderBlue =
-      generate_border_color_propertiy(generate_size: weight.size, rgba: border_blue)
+    gon_set_values.each do |key, value|
+      set_gon_variable_as(key: key, value: value)
+    end
   end
 
   private
 
   def generate_border_color_propertiy(generate_size:, rgba:)
     Array.new(generate_size) { rgba }
+  end
+
+  def set_gon_variable_as(key:, value:)
+    gon.public_send("#{key}=", value)
   end
 end
