42eddbc99a881e782117bde4312df73369873830
gouf <innocent.zero@gmail.com>
2020-08-09 16:10:23 +0900

    データベースから成長記録グラフを描画する
    サンプルとして `db/seeds.rb` に適当なデータを定義した
    `bundle e rails db:seed` コマンドで読み込みができる

--- app/controllers/growth_curve_sample_controller.rb
+++ app/controllers/growth_curve_sample_controller.rb
@@ -1,20 +1,15 @@
 class GrowthCurveSampleController < ApplicationController
   def index
-    # 仮で固定値
-    height = [46, 51, 55, 59, 62, 63, 64, 65.5, 66, 66.5, 71, 69.5]
-    # 12.times.map { (30..70).to_a.sample }
-    # gon.height = height
-    gon.height = 12.times.map { (40..70).to_a.sample }
+    growth_records = GrowthRecord.all
 
-    # NOTE: chart-js で値の数だけ色指定が必要
-    border_red = 'rgba(255, 0, 0, 1)'
-    gon.borderRed = Array.new(height.size) { border_red }
-
-    # -----------------------------------------
-
-    # 仮で固定値
-    weight = [2.26, 3.445, 4.62, 5, 6.05, 6.64, 7.1, 7.4, 7.5, 7.6, 7.8, 7.5]
-    gon.weight = weight
+    gon_set_values = {
+      # 身長・体重のマッピング
+      height: growth_records.map { |attr| attr.height },
+      weight: growth_records.map { |attr| attr.weight },
+      # 描画色の設定
+      borderRed: BORDER_COLOR_RED,
+      borderBlue: BORDER_COLOR_BLUE
+    }
 
     # NOTE: chart-js で値の数だけ色指定が必要
     border_blue = 'rgba(54, 162, 235, 1)'
