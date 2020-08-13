f4d74bc0cf28000bbb124cc158ef7f4dd0917601
gouf <innocent.zero@gmail.com>
2020-08-13 18:23:31 +0900

    参考リンクの URL を URL エンコード文字列に変換

--- spec/support/shared_contexts/gon.rb
+++ spec/support/shared_contexts/gon.rb
@@ -1,4 +1,4 @@
-# Ref: [How To: Test controllers with Rails (and RSpec) · heartcombo/devise Wiki](https://github.com/heartcombo/devise/wiki/How-To:-Test-controllers-with-Rails-(and-RSpec))
+# Ref: [How To: Test controllers with Rails (and RSpec) · heartcombo/devise Wiki](https://github.com/heartcombo/devise/wiki/How-To:-Test-controllers-with-Rails-%28and-RSpec%29)
 shared_context :gon do
   let(:gon) { RequestStore.store[:gon].gon }
   before { Gon.clear }
